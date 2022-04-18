492b95e314b2431f1e18bc72d984d1c1bf14ee04
kanikeva <kanikeva@4xxi.com>
2022-04-18 14:57:30 +0300

    Added base_page.py and main_page.py files

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,13 @@
 import time
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
+
 def test_guest_can_go_to_login_page(browser):
-        link = "http://selenium1py.pythonanywhere.com/"
         browser.implicitly_wait(10)
 
         browser.get(link)
-        login_link = browser.find_element_by_css_selector("#login_link")
-        login_link.click()
+        go_to_login_page(browser)
