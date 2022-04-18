7f4ec47eb878d74c0d4c722eb018b0ed31c3d2fd
kanikeva <kanikeva@4xxi.com>
2022-04-18 15:28:06 +0300

    Added should_be_login_link test

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,14 @@
 from .pages.main_page import MainPage
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/" 
-    browser.implicitly_wait(10)
 
     page = MainPage(browser,link) #intialise Page Object, add browser.driver example and url
-
     page.open() 
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
