5df5b84988901c50ef0f1c71916f7135e7bf2e99
kanikeva <kanikeva@4xxi.com>
2022-04-18 16:35:12 +0300

    Added login_page.py and updated ligin page checks with it

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,15 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_should_see_login_link(browser):
-
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
-
-    page = MainPage(browser,link) #intialise Page Object, add browser.driver example and url
+    page = MainPage(browser,link)
+    login_page = LoginPage() #intialise Page Object, add browser.driver example and url
     page.open() 
     page.go_to_login_page()
+    login_page.should_be_login_page()
