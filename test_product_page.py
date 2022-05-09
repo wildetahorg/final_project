1f6e51a42d7ee52457445b9369ddb08ded7cc964
kanikeva <kanikeva@4xxi.com>
2022-05-09 21:13:07 +0300

    Removed languages from links

--- test_product_page.py
+++ test_product_page.py
@@ -7,14 +7,14 @@ from .pages.basket_page import BasketPage
 class TestGuestSeeLoginLinkFromProductPage():
 
     def test_guest_should_see_login_link_on_product_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
         page.open()
         page.should_be_login_link()
 
     @pytest.mark.need_review
     def test_guest_can_go_to_login_page_from_product_page (self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
         page = ProductPage(browser, link)
         login_page = LoginPage(browser,browser.current_url)
 
@@ -37,21 +37,21 @@ class TestGuestAddToBasketFromProductPage():
 
     @pytest.mark.xfail
     def test_guest_cant_see_success_message_after_adding_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
         page.open()
         page.add_item_to_basket()
         page.there_is_no_success_message()
 
     def test_guest_cant_see_success_message(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
         page.open()
         page.there_is_no_success_message()
 
     @pytest.mark.xfail
     def test_message_disappeared_after_adding_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
         page.open()
         page.add_item_to_basket()
@@ -59,7 +59,7 @@ class TestGuestAddToBasketFromProductPage():
 
     @pytest.mark.need_review
     def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
         basket_page = BasketPage(browser, link)
 
@@ -72,7 +72,7 @@ class TestGuestAddToBasketFromProductPage():
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
         self.page = LoginPage(browser, link)
         self.page.open()
         self.page.register_new_user()
